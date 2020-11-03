816253738ff21c13f50bcf79c36f27949e48dd6f
Thiago <rp.thiagoluiz@gmail.com>
2020-11-03 16:31:23 -0300

    Pausa porq tive uma ideia

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -8,7 +8,7 @@ export default _ => {
 
    return (
       <div className="container-about">
-         
+         {/* Usar props aq. To querendo usar ela pra mudar o statado de um elemento ja existenti na tela. About vai mudando os icones das redes sociais de lugar conforme o maluco clica no bottom. */}
       </div>
    )
 }
